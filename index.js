4d81cd16d1a5c103925bd6d9cb3cfd5976944faa
StefanySantos <stefanyjenifer1@hotmail.com>
2021-11-16 20:08:06 -0300

    acrescentando o metodo post

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require("express"); //importando o express
 const app = express();
 const port = 8080; //server node
 
-//criando rotas
+//leia arquivos de json
+app.use(express.json());
+
+//criando rotas - endpoint
 //funçao callback pegando um dado
 app.get("/", function(req, res) {
     res.send("minha primeira requisição");
@@ -13,9 +16,19 @@ app.get("/segunda-req", function(req, res) {
 })
 
 app.get("/com-parametros", function(req, res) {
-    res.send('Com parametros funciona! bora viajar ' + req.query.nome);
+    if (req.query.nome === 'stefany') {
+        res.send("stefany chamou requisição")
+    }
+    res.send('Com parametros funciona! bora viajar ' + req.query.nome /*+ req.query.sobrenome*/ );
 })
 
+
+//post
+app.post("/meu-primeiro-post", function(req, res) {
+    //imprima requisições do body
+    console.log(req.body);
+    res.send("meu post funciona");
+});
 
 app.listen(port, function() {
     console.log("Ouvindo a porta: ", port);
