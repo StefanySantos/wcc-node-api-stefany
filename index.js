3ea88f0bdff2bca3c884bd649ef4cc136c373cae
StefanySantos <stefanyjenifer1@hotmail.com>
2021-11-16 21:36:13 -0300

    acrescentando o metodo put/delete

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { application } = require("express");
 const express = require("express"); //importando o express
 const app = express();
 const port = 8080; //server node
@@ -20,7 +21,7 @@ app.get("/com-parametros", function(req, res) {
         res.send("stefany chamou requisição")
     }
     res.send('Com parametros funciona! bora viajar ' + req.query.nome /*+ req.query.sobrenome*/ );
-})
+});
 
 
 //post
@@ -29,6 +30,18 @@ app.post("/meu-primeiro-post", function(req, res) {
     console.log(req.body);
     res.send("meu post funciona");
 });
+
+//put
+app.put("/meu-primeiro-put/:id", function(req, res) {
+    console.log(req.body, req.params.id);
+    res.send("meu put funciona");
+});
+
+//delete
+app.delete("/meu-primeiro-delete/:id", function(req, res) {
+    console.log(req.params.id);
+    res.send("meu delete funciona " + req.params.id)
+})
 
 app.listen(port, function() {
     console.log("Ouvindo a porta: ", port);
